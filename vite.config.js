9c8a4c611a00a88bd48689c2663a524117df59cd
duonglm38 <minhduong38@gmail.com>
2025-07-15 02:15:56 -0400

    fix url

--- vite.config.js
+++ vite.config.js
@@ -2,16 +2,18 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-const baseUrl = process.env.VITE_BASE_URL || '/';
+const REPO_NAME = 'thresh_mod';
 
 export default defineConfig({
-  plugins: [
-    vue()
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
-      vue$: 'vue/dist/vue.esm-bundler.js',
+    base: `/${REPO_NAME}/`,
+
+    plugins: [
+        vue()
+    ],
+    resolve: {
+        alias: {
+          '@': fileURLToPath(new URL('./src', import.meta.url)),
+          vue$: 'vue/dist/vue.esm-bundler.js',
     }
   },
 })
